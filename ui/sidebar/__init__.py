3b403c6d4b6899f03f3f15d3d90082c4ab5d48f1
OptikR <243767949+OptikR@users.noreply.github.com>
2025-11-20 15:51:57 +0100

    Add files via upload

---
+++ ui/sidebar/__init__.py
@@ -0,0 +1,9 @@
+"""
+Sidebar Module
+
+Sidebar components for the Real-Time Translation System.
+"""
+
+from .sidebar_widget import SidebarWidget
+
+__all__ = ['SidebarWidget']
---
+++ ui/toolbar/__init__.py
@@ -0,0 +1,9 @@
+"""
+Toolbar Module
+
+Toolbar components for the Real-Time Translation System.
+"""
+
+from .main_toolbar import MainToolbar
+
+__all__ = ['MainToolbar']
